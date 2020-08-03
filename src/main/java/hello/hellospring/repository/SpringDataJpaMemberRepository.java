5796af8e416e1616a5c42b6acca48c0b5e0134cb
이경진 <bramhome5848@gmail.com>
2020-08-03 19:48:32 +0900

    20200803 Spring DB Access

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -16,6 +16,8 @@ import java.util.Optional;
  * 접근 할 수 있는 메서드 범위 -> 상속 구조이기 때문에 부모 변수형태로 자식 객체를 가지고 있다고 해도 부모형에서 선언한 메서드만 접근 가능
  * SpringDataJpaMemberRepository 형의 변수로 service 에서 접근할 경우 JpaRepository 와 MemberRepository 에 공통으로 존재하는
  * 함수에 대해 어떤 함수를 접근할 것인지에 대한 에러또한 발생할 수 있음
+ * 일반 자바에서는 인터페이스 객체 생성을 위해서는 반드시 함수를 구현해야 하기 때문에 문제가 되지 않을 수 있지만
+ * JPA의 경우 구현체가 실행시에 생성되어 동작하기 떄문에 어떤 함수를 사용할 것인지를 명확히 지정해야 함, 함수 명은 겹치지 않도록 해야함..
  */
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
